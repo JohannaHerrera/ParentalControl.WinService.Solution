9c469de638e10eb3d06f85fbbdb8ec2c62c31a78
Kevin David Anchapaxi Moreta <kevin.anchapaxi@udla.edu.ec>
2021-11-25 17:05:06 -0500

    Bloqueo Web

--- ParentalControl.WinService.WinServiceLib/Server/WinServices/ParentalControl/Engines/Processors/WebLock/Processor.cs
+++ ParentalControl.WinService.WinServiceLib/Server/WinServices/ParentalControl/Engines/Processors/WebLock/Processor.cs
@@ -2,18 +2,24 @@
 using ParentalControl.WinService.WinServiceLib.Server.WinServices.ParentalControl.Engines.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ParentalControl.WinService.Business;
 namespace ParentalControl.WinService.WinServiceLib.Server.WinServices.ParentalControl.Engines.Processors.WebLock
 {
     internal class Processor : BaseProcessor
     {
-
+        
         #region public override properties
 
         public override string Name { get { return ServiceConfiguration.WebLock; } }
+        int i = 1;
+        public void block(int i)
+        {
+
+        }
 
         #endregion
 
