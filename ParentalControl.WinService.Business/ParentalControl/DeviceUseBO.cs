28bb312fd383679c0abc8bce9addff36ea452909
Kevin David Anchapaxi Moreta <kevin.anchapaxi@udla.edu.ec>
2021-11-26 11:41:10 -0500

    Cambios 2

---
+++ ParentalControl.WinService.Business/ParentalControl/DeviceUseBO.cs
@@ -0,0 +1,51 @@
+﻿using ParentalControl.WinService.Data;
+using ParentalControl.WinService.Models.Device;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParentalControl.WinService.Business.ParentalControl
+{
+    public class DeviceUseBO
+    {
+        /// <summary>
+        /// Obtiene el uso del dispositivo para un día especificado
+        /// </summary>
+        /// <param name="infantId"></param>
+        /// <param name="dia"></param>
+        /// <returns></returns>
+        public List<DeviceUseModel> GetDeviceUse(int infantId, string dia)
+        {
+            string query = $"SELECT * FROM DeviceUse WHERE InfantAccountId = '{infantId}'" +
+                           $" AND DeviceUseDay = '{dia}'";
+
+            List<DeviceUseModel> deviceUseModelList = this.ObtenerListaSQL<DeviceUseModel>(query).ToList();
+
+            return deviceUseModelList;
+        }
+
+        /// <summary>
+        /// Método para convertir una lista DataTable a un TModel (Modelo genérico)
+        /// </summary>
+        /// <param name="TModel"></param>
+        /// <returns>IList<TModel></returns>
+        private IList<TModel> ObtenerListaSQL<TModel>(string query)
+        {
+            try
+            {
+                DataTable dataTableInformacion = SQLConexionDataBase.Query(query);
+                var listaResultante = dataTableInformacion.MapDataTableToList<TModel>();
+
+                return listaResultante;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ ParentalControl.WinService.Business/ParentalControl/ScheduleBO.cs
@@ -0,0 +1,52 @@
+﻿using ParentalControl.WinService.Data;
+using ParentalControl.WinService.Models.ScheduleAccount;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParentalControl.WinService.Business.ParentalControl
+{
+    public class ScheduleBO
+    {
+        public ScheduleModel GetSchedule(int scheduleId)
+        {
+            string query = $"SELECT * FROM Schedule WHERE ScheduleId = {scheduleId}";
+            List<ScheduleModel> scheduleModelList = this.ObtenerListaSQL<ScheduleModel>(query).ToList();
+            ScheduleModel scheduleModel = scheduleModelList.FirstOrDefault();
+
+            return scheduleModel;
+        }
+
+        public bool CompareScheduleWithSystemTime(int scheduleId)
+        {
+            ScheduleModel scheduleModel = GetSchedule(scheduleId);
+            DateTime horaInicio = scheduleModel.ScheduleStartTime;
+            DateTime horaFin = scheduleModel.ScheduleEndTime;
+            string horaActual = DateTime.Now.ToString("HH:mm");
+            DateTime.ParseExact(horaActual, "HH:mm", null);
+
+            if (horaActual.CompareTo(horaInicio) >= 0 && horaActual.CompareTo(horaFin) <= 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        private IList<TModel> ObtenerListaSQL<TModel>(string query)
+        {
+            try
+            {
+                DataTable dataTableInformacion = SQLConexionDataBase.Query(query);
+                var listaResultante = dataTableInformacion.MapDataTableToList<TModel>();
+
+                return listaResultante;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
